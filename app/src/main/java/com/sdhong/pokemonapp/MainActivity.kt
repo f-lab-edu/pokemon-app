f3eb1fd6a678c3d8cd49797737e18eef4f6faf7f
Seongdeok Hong <sdhong0609@gmail.com>
2025-02-23 00:23:28 +0900

    feat: 두 개의 탭에 각각 아이콘 추가

--- app/src/main/java/com/sdhong/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/sdhong/pokemonapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.sdhong.pokemonapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sdhong.pokemonapp.databinding.ActivityMainBinding
 
@@ -36,6 +38,11 @@ class MainActivity : AppCompatActivity() {
                 0 -> getString(R.string.all_pokemon_tab_title)
                 1 -> getString(R.string.history_tab_title)
                 else -> ""
+            }
+            tab.icon = when (position) {
+                0 -> AppCompatResources.getDrawable(this, R.drawable.ic_pikachu)
+                1 -> AppCompatResources.getDrawable(this, R.drawable.ic_pokeball)
+                else -> null
             }
         }.attach()
     }
