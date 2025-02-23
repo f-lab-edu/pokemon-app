614599ace7f1c6dfce0b2ffaf3fc844edd815ae8
Seongdeok Hong <sdhong0609@gmail.com>
2025-02-23 19:49:17 +0900

    build: coil3로 version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    implementation(libs.coil)
+    implementation(libs.coil3.coil)
+    implementation(libs.coil.network.okhttp)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
