a5c990b1ec71f5e4d379901e1dc0571950ec1a82
Seongdeok Hong <sdhong0609@gmail.com>
2025-02-23 19:49:17 +0900

    build: coil3로 version update

--- app/src/main/java/com/sdhong/pokemonapp/MainViewHolder.kt
+++ app/src/main/java/com/sdhong/pokemonapp/MainViewHolder.kt
@@ -1,7 +1,7 @@
 package com.sdhong.pokemonapp
 
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import coil.load
+import coil3.load
 import com.sdhong.pokemonapp.databinding.ItemPokemonBinding
 import com.sdhong.pokemonapp.model.Pokemon
 
