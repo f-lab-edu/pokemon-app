0409fb0e54d0e5bea0517d709ea0e91ad06b4ed3
Seongdeok Hong <sdhong0609@gmail.com>
2025-02-23 20:36:37 +0900

    feat: 포켓몬 카드 리스트 테두리 디자인 및 spacing

--- app/src/main/java/com/sdhong/pokemonapp/AllPokemonFragment.kt
+++ app/src/main/java/com/sdhong/pokemonapp/AllPokemonFragment.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.sdhong.pokemonapp.databinding.FragmentAllPokemonBinding
-import com.sdhong.pokemonapp.model.Pokemon
 import com.sdhong.pokemonapp.model.allPokemons
 
 class AllPokemonFragment : Fragment() {
@@ -26,7 +26,23 @@ class AllPokemonFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.recyclerViewAllPokemon.adapter = mainAdapter
+        binding.recyclerViewAllPokemon.run {
+            adapter = mainAdapter
+            layoutManager = GridLayoutManager(context, DEFAULT_SPAN_COUNT)
+            addItemDecoration(
+                GridSpacingItemDecoration(
+                    spanCount = DEFAULT_SPAN_COUNT,
+                    spacing = DEFAULT_GRID_SPACING,
+                    includeEdge = true
+                )
+            )
+        }
+
         mainAdapter.submitList(allPokemons)
+    }
+
+    companion object {
+        private const val DEFAULT_SPAN_COUNT = 2
+        private const val DEFAULT_GRID_SPACING = 50
     }
 }
