9480b12b8f91da4c525fecc5cd4437b94ecbc02a
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-09 00:45:42 +0900

    build: 필요없는 라이브러리 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,6 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
 
     testImplementation(libs.junit.jupiter)
-    testImplementation(libs.mockito.kotlin)
-    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
