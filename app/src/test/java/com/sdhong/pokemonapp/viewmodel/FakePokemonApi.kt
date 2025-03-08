00910520911d0cece3b888c05d6b8db7e74f99a7
Seongdeok Hong <sdhong0609@gmail.com>
2025-03-08 21:31:39 +0900

    test: DetailViewModelTest
    refactor: DetailViewModelTest expected, actual 변경
    
    test: DetailViewModelTest 수정
    
    refactor: DetailViewModelTest 수정
    
    test: DetailViewModelTest

---
+++ app/src/test/java/com/sdhong/pokemonapp/viewmodel/FakePokemonApi.kt
@@ -0,0 +1,94 @@
+package com.sdhong.pokemonapp.viewmodel
+
+import com.sdhong.pokemonapp.remote.model.PokemonDetailResponse
+import com.sdhong.pokemonapp.remote.model.PokemonListResponse
+
+class FakePokemonApi {
+
+    private val pokemonDetailList = listOf(
+        PokemonDetailResponse(
+            name = "bulbasaur",
+            weight = 69,
+            height = 7,
+            types = listOf(
+                PokemonDetailResponse.Type(
+                    type = PokemonDetailResponse.TypeInfo(
+                        name = "grass"
+                    )
+                ),
+                PokemonDetailResponse.Type(
+                    type = PokemonDetailResponse.TypeInfo(
+                        name = "poison"
+                    )
+                )
+            ),
+            abilities = listOf(
+                PokemonDetailResponse.Ability(
+                    ability = PokemonDetailResponse.AbilityInfo(
+                        name = "overgrow"
+                    )
+                ),
+                PokemonDetailResponse.Ability(
+                    ability = PokemonDetailResponse.AbilityInfo(
+                        name = "chlorophyll"
+                    )
+                )
+            ),
+            sprites = PokemonDetailResponse.Sprites(
+                imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            )
+        ),
+        PokemonDetailResponse(
+            name = "ivysaur",
+            weight = 130,
+            height = 10,
+            types = listOf(
+                PokemonDetailResponse.Type(
+                    type = PokemonDetailResponse.TypeInfo(
+                        name = "grass"
+                    )
+                ),
+                PokemonDetailResponse.Type(
+                    type = PokemonDetailResponse.TypeInfo(
+                        name = "poison"
+                    )
+                )
+            ),
+            abilities = listOf(
+                PokemonDetailResponse.Ability(
+                    ability = PokemonDetailResponse.AbilityInfo(
+                        name = "overgrow"
+                    )
+                ),
+                PokemonDetailResponse.Ability(
+                    ability = PokemonDetailResponse.AbilityInfo(
+                        name = "chlorophyll"
+                    )
+                )
+            ),
+            sprites = PokemonDetailResponse.Sprites(
+                imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
+            )
+        ),
+    )
+
+    fun getAllPokemon(): PokemonListResponse {
+        return PokemonListResponse(
+            next = "",
+            results = listOf(
+                PokemonListResponse.PokemonListItem(
+                    name = "bulbasaur",
+                    url = "https://pokeapi.co/api/v2/pokemon/1/"
+                ),
+                PokemonListResponse.PokemonListItem(
+                    name = "ivysaur",
+                    url = "https://pokeapi.co/api/v2/pokemon/2/"
+                )
+            )
+        )
+    }
+
+    fun getImgUrl(id: Int): String = pokemonDetailList[id - 1].sprites.imgUrl
+
+    fun getPokemonDetail(id: Int): PokemonDetailResponse = pokemonDetailList[id - 1]
+}
